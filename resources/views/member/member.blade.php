ee57d6ba5f467a126521b1b7c68210154322274d
Hoang <apple@APPLEs-MacBook-Pro.local>
2020-02-13 18:03:15 +0700

    CRUD member

---
+++ resources/views/member/member.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.admin')
+@section('title', 'Members')
+@section('management', 'Manager Members')
+
+@section('content')
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+    <a type="button" href="{{ route('member.create') }}" class="btn btn-success">
+        <span class="fa fa-edit mr-2"></span>Add
+    </a>
+    <br>
+    <table class="table table-sm table-striped table-hover bg-light">
+        <br>
+        <tr class="thead-dark">
+            <th class="text-center">ID</th>
+            <th class="text-center">Name</th>
+            <th class="text-center">Email</th>
+            <th class="text-center">Phone</th>
+            <th class="text-center">Address</th>
+            <th class="text-center">Username</th>
+            <th class="text-center">Password</th>
+            <th class="text-center">Image</th>
+            <th class="text-center">Is_admin</th>
+            <th class="text-center"></th>
+        </tr>
+        @foreach ($members as $member)
+        <tr>
+            <td class="text-center">{{ $member->id }}</td>
+            <td class="text-center">{{ $member->name }}</td>
+            <td class="text-center">{{ $member->email }}</td>
+            <td class="text-center">{{ $member->phone }}</td>
+            <td class="text-center">{{ $member->address }}</td>
+            <td class="text-center">{{ $member->username }}</td>
+            <td class="text-center">{{ $member->password }}</td>
+            <td class="text-center">{{ $member->image }}</td>
+            <td class="text-center">{{ $member->is_admin }}</td>
+            <td class="text-center">
+                <a type="button" href="{{ route ('member.edit', $member->id) }}" class="btn btn-warning">
+                    <span class="fa fa-edit mr-2"></span>Edit
+                </a>
+                &nbsp;
+                <form action="{{ route('member.destroy', $member->id) }}" method="post">
+                    @method('delete')
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+</div>
+@endsection
