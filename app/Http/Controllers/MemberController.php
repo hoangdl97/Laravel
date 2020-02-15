462220a59301af952ebed2985e19a93b07cd705f
macbook <macbook@macbooks-MacBook-Pro.local>
2020-02-16 00:16:45 +0700

    CRUD member 16/2

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Member;
+use App\Http\Requests\RegisterMemberRequest;
+
+class MemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $members = Member::all();
+        return view('member.member')->with('members', $members);
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('member.create');
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(RegisterMemberRequest $request)
+    {
+        // dd(12);
+        $result = new Member();
+        $result = Member::create($request->all());
+
+        return redirect()->route('member.index');
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('member.edit')->with('members', Member::findOrFail($id));
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(RegisterMemberRequest $request, $id)
+    {
+        $result = Member::findOrFail($id);
+        $result->update($request->all());
+
+        return redirect()->route('member.index');
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $result = Member::findOrFail($id);
+        $result->delete();
+        return redirect()->route('member.index');
+        //
+    }
+}
